9f2cc3ee044cab1e3ebb02fd0d9a1d50e2404755
Edv-Fendi <edvaldo.defendi@gmail.com>
2024-08-04 00:46:13 -0300

    useContext + useReduces examples

--- src/App.js
+++ src/App.js
@@ -1,41 +1,69 @@
-import React, { useReducer } from 'react';
-
+import React, { createContext, useContext, useReducer, useRef } from 'react';
+import P from 'prop-types';
 import './App.css';
 
+//actions.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
+  INCREMENTAR_CONTADOR: 'INCREMENTAR_CONTADOR',
+};
+
+//data.js
 export const globalState = {
   title: 'O titulo do contexto blablabla',
   body: 'O body do contexto',
   counter: 0,
 };
-const reducer = (state, action) => {
+
+//reducer.js
+export const reducer = (state, action) => {
+  console.log(action);
   switch (action.type) {
-    case 'muda': {
-      console.log('Mudou o titulo no dia: ', action.payload);
-      return { ...state, title: `Novo Titulo: ${action.payload}` };
-    }
-    case 'contador': {
+    case actions.CHANGE_TITLE:
+      return { ...state, title: action.payload };
+    case actions.INCREMENTAR_CONTADOR:
       return { ...state, counter: state.counter + 1 };
-    }
-    case 'inverter': {
-      const { title } = state;
-      return { ...state, title: title.split('').reverse().join('') };
-    }
   }
   return { ...state };
 };
+//AppContext.jsx
+export const Context = createContext();
+export const AppContext = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, globalState);
+
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload });
+  };
+
+  return <Context.Provider value={{ state, changeTitle }}>{children}</Context.Provider>;
+};
+
+AppContext.propTypes = {
+  children: P.node,
+};
+
+//H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
+  const {
+    state: { title },
+  } = context;
+  return (
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>{title}</h1>
+      <input type="text" ref={inputRef} placeholder="Insira seu Texto" />
+    </>
+  );
+};
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, globalState);
-  const { title, body, counter } = state;
   return (
-    <div className="App">
-      <h1>
-        {title}: {counter}
-      </h1>
-      <button onClick={() => dispatch({ type: 'muda', payload: new Date().toLocaleString('pt-BR') })}>Contador</button>
-      <button onClick={() => dispatch({ type: 'contador' })}>Contador</button>
-      <button onClick={() => dispatch({ type: 'inverter' })}>Inverter</button>
-    </div>
+    <AppContext>
+      <div className="App">
+        <H1 />
+      </div>
+    </AppContext>
   );
 }
 
